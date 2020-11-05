db12efcaf4a328cea4f46d83817dd648ba6f545a
Anvahi <undeadhedg@gmail.com>
2020-11-05 17:37:17 +0300

    classes in different files in net4

---
+++ Net_lab4/Clients.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Net_lab4_classes;
+
+namespace Net_lab4_classes {
+    /// <summary>
+    /// класс клиент
+    /// </summary>
+    public class Clients {
+        /// <summary>
+        /// Название
+        /// </summary>
+        public string Name { get; set; } = "";
+        /// <summary>
+        /// Вид собственности
+        /// </summary>
+        public string PropertyType { get; set; } = "";
+        /// <summary>
+        /// адрес
+        /// </summary>
+        public string Adress { get; set; } = "";
+        /// <summary>
+        /// телефон
+        /// </summary>
+        public string Phone { get; set; } = "";
+        /// <summary>
+        /// Контактное лицо
+        /// </summary>
+        public string ContactPerson { get; set; } = "";
+
+        public Clients() { }
+
+        public Clients(string _Name, string _PropertyType, string _Adress, string _Phone, string _ContactPerson) {
+            this.Name = _Name;
+            this.PropertyType = _PropertyType;
+            this.Adress = _Adress;
+            this.Phone = _Phone;
+            this.ContactPerson = _ContactPerson;
+        }
+
+        public bool IsValid {
+            get {
+                if (String.IsNullOrWhiteSpace(Name))
+                    return false;
+                if (String.IsNullOrWhiteSpace(PropertyType))
+                    return false;
+                if (String.IsNullOrWhiteSpace(Adress))
+                    return false;
+                if (String.IsNullOrWhiteSpace(Phone))
+                    return false;
+                if (String.IsNullOrWhiteSpace(ContactPerson))
+                    return false;
+                return true;
+            }
+        }
+
+        public override string ToString() {
+            return $"Название: { Name }\r\nВид собственности: { PropertyType }\r\nАдрес: { Adress }\r\nТелефон: { Phone }\r\n Контактное лицо: {ContactPerson}";
+        }
+    }
+}
---
+++ Net_lab4/Credits.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Net_lab4_classes;
+
+namespace NET_lab4_classes  {
+    /// <summary>
+    /// Кредиты
+    /// </summary>
+    public class Credits : IValidatable
+    {
+        /// <summary>
+        /// Название кредита
+        /// </summary>
+        public TypesOfCredits name { get; set; } = new TypesOfCredits();
+        /// <summary>
+        /// Клиент
+        /// </summary>
+        public Clients client { get; set; } = new Clients();
+        /// <summary>
+        /// сумма
+        /// </summary>
+        public double sum { get; set; } = 0.0;
+        /// <summary>
+        /// дата выдачи
+        /// </summary>
+        public DateTime date { get; set; } = DateTime.Today;
+
+        public Credits() { }
+
+        public Credits(TypesOfCredits _name, Clients _client, double _sum, DateTime _date) {
+            this.name = _name;
+            this.client = _client;
+            this.sum = _sum;
+            this.date = _date;
+        }
+
+        public bool IsValid {
+            get {
+                if (name == null)
+                    return false;
+                if (client == null)
+                    return false;
+                if (sum <= 0)
+                    return false;
+                if (date > DateTime.Today)
+                    return false;
+                return true;
+            }
+        }
+
+        public override string ToString() {
+            return $"Название: { name }\r\nКлиент: { client }\r\nСумма: { sum }\r\nДата выдачи: { date }\r\n";
+        }
+    }
+}
