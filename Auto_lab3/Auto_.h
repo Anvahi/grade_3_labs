c4bc40c3618129a2fbbc04cb1cc79e77056bd4cf
Anvahi <undeadhedg@gmail.com>
2020-11-05 17:30:52 +0300

    auto 3 with spaces

---
+++ Auto_lab3/Auto_.h
@@ -0,0 +1,16 @@
+#ifndef AUTO_.H
+
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <stdlib.h>
+using namespace std;
+
+string input();
+string spacing(string read);
+vector <string> table_id(string reread);
+string _id(string reread, vector <string> table);
+void output_id_table(vector <string> table);
+
+#endif // !AUTO_.H
